a2e4afb481fe7cb3edddf9e4395a4e455832e4df
LoudAsh21 <thiagosancho2006@gmail.com>
2025-03-23 18:13:55 -0300

    testando conexao no banco de dados ao iniciar o servidor

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import courseRoutes from "./routes/courseRoutes.js";
 import dotenv from "dotenv";
-import { requestLogger } from "./config/logging.js";
+import { logError, logInfo, logWarn, requestLogger } from "./config/logging.js";
+import { Connect, Query } from "./config/database.js";
 dotenv.config();
 
 const router = express();
@@ -11,7 +12,37 @@ router.use(requestLogger);
 router.use("/repository", courseRoutes);
 
 router.listen(process.env.API_PORT, () => {
-  console.log(
-    `Servidor rodando na porta ${process.env.API_PORT}: ${process.env.API_URL}`
-  );
+  logInfo("INDEX", "Testando conexão ao banco de dados...");
+
+  Connect()
+    .then((connection) => {
+      Query(connection, "SELECT * FROM course WHERE id_course=1;").then((value) => {
+        logInfo("INDEX", "Servidor conectado ao banco de dados!");
+        console.log(
+          `Servidor rodando na porta ${process.env.API_PORT}: ${process.env.API_URL}`
+        );
+        connection.end();
+      })
+        .catch((error) => {
+          logError(
+            "INDEX",
+            "Ocorreu um erro ao conectar ao banco de dados!",
+            error
+          );
+          logWarn("INDEX", "Fechando o servidor!")
+          process.exit();
+        })
+        .finally(() => {
+          connection.end();
+        })
+    })
+    .catch((error) => {
+      logError(
+        "INDEX",
+        "Ocorreu um erro ao conectar ao banco de dados!",
+        error
+      );
+      logWarn("INDEX", "Fechando o servidor!")
+      process.exit();
+    })
 });
